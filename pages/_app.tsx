fc574e9f1819076f6baeb3d79be9f4629a1d2444
Arina <arinashkapa@yandex.ru>
2026-06-02 21:54:39 +0200

    Добавила отчеты проверки доступности и производительности

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import { SessionProvider } from 'next-auth/react'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
+      <Head>
+        <title>EduFlow - персональные рекомендации курсов</title>
+        <meta name="description" content="MVP интерфейса персонализированных рекомендаций образовательных курсов." />
+      </Head>
       <Component {...pageProps} />
     </SessionProvider>
   )
